e02cea672b8aabbda11a0d95b0002117b9edd868
pengju1218 <920988751@qq.com>
2016-09-09 11:06:24 +0800

    Signed-off-by: pengju1218 <920988751@qq.com>

---
+++ csdnTabLayout/app/src/com/example/tangyangkai/testdemo/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.tangyangkai.testdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by tangyangkai on 16/5/10.
+ */
+public class LoginActivity extends AppActivity {
+
+    private String username;
+    @Override
+    protected void handleIntent(Intent intent) {
+        super.handleIntent(intent);
+        Bundle bundle = intent.getExtras();
+        if (null != bundle) {
+            username = bundle.getString("username");
+        }
+    }
+
+    @Override
+    protected BaseFragment getFirstFragment() {
+        return FirstFragment.newInstance(username);
+    }
+
+}
+
---
+++ csdnTabLayout/app/src/com/example/tangyangkai/testdemo/ThirdFragment.java
@@ -0,0 +1,45 @@
+package com.example.tangyangkai.testdemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by tangyangkai on 16/5/10.
+ */
+public class ThirdFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_third;
+    }
+
+    private ImageView backIMg;
+
+    public static ThirdFragment newInstance() {
+
+        return new ThirdFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+
+        backIMg = (ImageView) view.findViewById(R.id.third_back);
+        backIMg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeFragment();
+            }
+        });
+
+    }
+
+
+}
